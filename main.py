1e37b3bc608eaccc58a80615715aa306517e8740
Maizuuuu <daniil.gameacc@gmail.com>
2025-02-02 19:56:32 +0700

    megakinipoisk 1.0

---
+++ main.py
@@ -0,0 +1,67 @@
+from flask import Flask, render_template
+import sqlite3
+
+app = Flask("main")
+db_name = "films.db"
+
+
+class Film:
+    def __init__(
+        self,
+        id,
+        title,
+        year,
+        genres,
+        country,
+        description,
+        duration,
+        rating,
+        age_rating,
+        poster,
+    ):
+        self.id = id
+        self.title = title
+        self.year = year
+        self.genres = genres
+        self.country = country
+        self.description = description
+        self.duration = duration
+        self.rating = rating
+        self.age_rating = age_rating
+        self.poster = poster
+
+
+def get_films_list(page=1, limit=25, offset=25):
+    con = sqlite3.connect(db_name)
+    sql = """SELECT * FROM movie
+    LIMIT ? OFFSET ?"""
+    q = con.execute(sql, [limit, (page - 1) * offset])
+    data = q.fetchall()
+    return [Film(*d) for d in data]
+
+def get_film_by_id(id):
+    con = sqlite3.connect(db_name)
+    sql = """SELECT * FROM movie
+    WHERE id = ?"""
+    q = con.execute(sql, [id])
+    data = q.fetchone()
+    return Film(*data)
+
+@app.route("/")
+def home():
+    films = get_films_list()
+    return render_template("index.html", films=films, page =1)
+
+@app.route("/pages/<int:page>")
+def pages(page):
+    films = get_films_list(page)
+    return render_template("index.html", films=films, page=page)
+
+
+@app.route("/films/<int:id>")
+def film(id):
+    film = get_film_by_id(id)
+    return render_template("film.html", film = film)
+
+
+app.run(host="0.0.0.0", port=8080, debug=True)
